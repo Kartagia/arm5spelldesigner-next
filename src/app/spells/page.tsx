f176d7f27d83c1c8fd487c3efda90400b206cdda
Antti Kautiainen <antti@kautiainen.com>
2025-03-11 09:51:10 +0200

    feat: page spells
    The page spells showing spells.
    
    Changes to be committed:
    	modified:   src/app/spells/page.tsx
        - feat: Page spells showing list of spells.

--- src/app/spells/page.tsx
+++ src/app/spells/page.tsx
@@ -1,8 +1,84 @@
 
+import { ArtKey, RDTInfo, SpellRequisite } from "@/data/spells";
+import Spell from "../../components/Spell";
+import { ReactElement } from "react";
 
 export default function Spells() {
 
-    const spells = [];
+    const spells : Array<ReactElement> = [
+        (<Spell key={`Demons eternal oblivion(Generic)`} name="Demons eternal oblivion" level="Generic" requisites={[] as SpellRequisite[]}
+        ranges={
+            [
+                {
+                    name: "Voice",
+                    modifier: 2,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        durations={
+            [
+                {
+                    name: "Momentary",
+                    modifier: 0,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        targets={
+            [
+                {
+                    name: "Individual",
+                    modifier: 0,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        isGeneric={true}
+        
+        mode={"card"}
+        />),
+        (<Spell key={`Cloak of Ducks Feathers`} name="Cloak of Ducks Feathers" level={4} 
+            requisites={[
+                {
+                    requisite: "required",
+                    value: 0,
+                    art: new ArtKey("An")
+                }
+            ] as SpellRequisite[]}
+        ranges={
+            [
+                {
+                    name: "Voice",
+                    modifier: 2,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        durations={
+            [
+                {
+                    name: "Momentary",
+                    modifier: 0,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        targets={
+            [
+                {
+                    name: "Individual",
+                    modifier: 0,
+                    secondaryRDTs: []
+                } as RDTInfo 
+            ]
+        }
+        isGeneric={true}
+        
+        mode={"card"}
+        />)
+         
+    ];
 
     return (<div>
         <h1>Spells</h1>
