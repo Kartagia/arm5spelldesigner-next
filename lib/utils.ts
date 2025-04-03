f93776d4f19e445bea1950d0fd36e5371ac59b36
Antti Kautiainen <antti@kautiainen.com>
2025-04-03 12:02:17 +0300

    feat: module check and checkoptions
    The check callback.
    
    Changes to be committed:
    	modified:   lib/utils.ts
        - feat: interface CheckOptions<TYPE>
          The options for checking a alue of type TYPE.
        - feat: type Check<TYPE>(value: any, options: CheckOptions<TYPE>): TYPE
          The callback to check a value.

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,35 @@
 /**
  * Utilities library.
  */
+
+
+/**
+ * The opitons for checking values.
+ */
+
+export interface CheckOptions<TYPE> {
+
+    /**
+     * The error message of the failed check.
+     */
+    message?: string;
+
+    /**
+     * The smallest accepted value.
+     */
+    minValue?: TYPE;
+
+    /**
+     * The largest accepted value.
+     */
+    maxValue?: TYPE;
+}/**
+ * Check value.
+ * @param value The tested value.
+ * @param options The optional options of the check.
+ * @returns The valid value derived from the value.
+ * @throws {SyntaxError} The value was invalid.
+ */
+
+export type Check<TYPE> = (value: any, options?: CheckOptions<TYPE>) => TYPE;
+
