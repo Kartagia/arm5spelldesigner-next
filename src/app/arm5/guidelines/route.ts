8da8c0f2a260ed099cf49d2132a9c406c733ffe0
Antti Kautiainen <antti@kautiainen.com>
2025-04-03 12:05:51 +0300

    route: POST /arm5/guidelines
    The create a new guideline route.
    
    Changes to be committed:
    	modified:   src/app/arm5/guidelines/route.ts
        - feat: function checkTitle
        - feat: function chechLevel
        - feat: function checkArt
        - feat: function checkDescription
        - feat: function parseJsonGuideline

--- src/app/arm5/guidelines/route.ts
+++ src/app/arm5/guidelines/route.ts
@@ -1,8 +1,17 @@
-import { Spell, SpellGuideline } from "@/data/spells";
+import { ArtKey, Level, Spell, SpellGuideline } from "@/data/spells";
 import { NextRequest, NextResponse } from "next/server";
 import { EntryFilter, Identified, createIdentified, Predicate } from "@/data/utils";
+import { Check } from "../../../../lib/utils";
+import { CheckOptions } from "../../../../lib/utils";
 import { AccessMethods } from "@/data/api_keys";
 import { validApiKey, validApiReadKey, validApiRouteKey } from "@/data/config_api"
+import ErrorResponse from "@/api/ErrorResponse";
+import { GUID } from "@/data/guid";
+import { ArtReference, validJournalDate } from "@/api/SpellGuidelineResponse";
+import { promised } from "@/lib/utils";
+import { fetchArt } from "@/data/artData";
+import { TrueValidator } from "@/data/dao";
+import Art from "@/data/arts";
 /**
  * Test authentication satatus of the request.
  * @param request The tested request.
@@ -204,6 +213,144 @@ class SpellGuidelineFilter {
     }
 }
 
+
+/**
+ * @type {Check<Level>}
+ */
+export function checkLevel(value: any, options: CheckOptions<Level> = {}): Level {
+    const { message = "Invalid level", minValue = 1, maxValue = 255 } = options;
+    switch (typeof value) {
+        case "number":
+            if (Number.isSafeInteger(value) && (typeof minValue !== "number" || minValue <= value)
+                && (typeof maxValue !== "number" || value <= maxValue)) {
+                return value;
+            } else {
+                throw new SyntaxError(message, { cause: "Invalid numeric value" });
+            }
+        case "string":
+            if (value === "Generic" || value === "General") {
+                return "Generic";
+            }
+        default:
+            throw new SyntaxError(message);
+    }
+}
+
+function checkTitle(value: any, options: CheckOptions<string> = {}): string {
+    const { message = "Invalid title" } = options;
+    var match;
+    if (typeof value === "string" && /^[A-Z+-](?:[\w+-])*?(?:,?\s[\w+-]*)*\.(?:$|\s*?)$/.test(value)) {
+        return value.trim();
+    } else {
+        throw new SyntaxError(message);
+    }
+}
+
+function checkDescription(value: any, options: CheckOptions<string> = {}): string | undefined {
+    const { message = "Invalid description" } = options;
+    if (["string", "undefined"].some(type => (typeof value === type))) {
+        return value;
+    } else {
+        throw new SyntaxError(message);
+    }
+}
+
+export interface ArtCheckOptions {
+    /**
+     * The expected style.
+     */
+    style?: string;
+
+    /**
+     * The expected type.
+     */
+    type?: string;
+}
+
+function checkArt(value: any, options: CheckOptions<ArtKey> & ArtCheckOptions = {}): ArtKey {
+    const { message = "Invalid art" } = options;
+    const filter = (art: Art) => {
+        if (options.style !== undefined && art.style !== options.style) {
+            return false;
+        } else if (options.type !== undefined && art.type !== options.type) {
+            return false;
+        }
+        return true;
+    }
+    if (typeof value === "object") {
+        const refProps: [string, (value: any) => boolean][] = [
+            ["type", (value: any) => (typeof value === "string" && ["technique", "form"].some(valid => (valid === value)))],
+            ["guid", (value: any) => (typeof value === "string" && GUID.GUIDRegex().test(value))],
+            ["currentDate", (value: any) => (typeof value === "string" && validJournalDate(value))]
+        ];
+        if (refProps.every(([prop, validator]) => (prop in value && validator(value[prop])))) {
+            // The value is reference.
+            const result = promised(fetchArt(value).then((art) => {
+                if (filter(art)) {
+                    return art.abbrev;
+                } else {
+                    undefined;
+                }
+            }))
+            if (result) {
+                return result;
+            } else {
+                throw new SyntaxError(message);
+            }
+        } else {
+            throw new SyntaxError(message);
+        }
+    } else if (typeof value === "string") {
+        if (GUID.GUIDRegex().test(value)) {
+            // We did get GUID.
+            const result = promised(fetchArt(value).then((art) => {
+                if (filter(art)) {
+                    return art.abbrev;
+                } else {
+                    return undefined;
+                }
+            }));
+            if (result) {
+                return result;
+            } else {
+                throw new SyntaxError(message);
+            }
+        } else {
+            // We do have art or art reference.
+            const result = promised(fetchArt(value).then((art) => (art.abbrev)));
+            if (result) {
+                return result;
+            } else {
+                throw new SyntaxError(message);
+            }
+        }
+    }
+
+    throw new SyntaxError(message);
+}
+
+/**
+ * Parse JSON guideline.
+ * @param jsonBody The JSON body.
+ * @returns The promise of a parsed spell guideline.
+ * @throws {SyntaxError} The rejected json body did not contain a valid promise.
+ */
+async function parseJsonGuideline(jsonBody: any): Promise<SpellGuideline> {
+
+    if (typeof jsonBody === "object") {
+
+        return {
+            name: checkTitle(jsonBody.title),
+            description: checkDescription(jsonBody.description),
+            level: checkLevel(jsonBody.level),
+            technique: checkArt(jsonBody.technique, { type: "technique" }),
+            form: checkArt(jsonBody.form, { type: "form" })
+        };
+    } else {
+        throw new SyntaxError("Invalid json body for a guideline");
+    }
+}
+
 /**
  * Parse a JSON requuest
  * @param jsonBody The jason request.
@@ -286,8 +433,8 @@ async function parseJsonRequest(jsonBody: any): Promise<SpellGuidelineOptions> {
  * @returns True, if and only if the request is allowed for the route.
  */
 export function canAccess(request: NextRequest, route: string = "/spellguidelines"): boolean {
-    var method: AccessMethods= "GET";
-    var apiKey: string|null = null;
+    var method: AccessMethods = "GET";
+    var apiKey: string | null = null;
     switch (request.method) {
         case "POST":
             method = "CREATE";
@@ -313,21 +460,12 @@ export function canAccess(request: NextRequest, route: string = "/spellguideline
 }
 
 /**
- * Get filtered guidelines.
- * @param request 
+ * Create entry filter.
+ * @param request The request.
+ * @returns The promise of the entry filter.
+ * @throws {ErrorResponse} The rejected error response describing the erroneous request.
  */
-export async function POST(request: NextRequest) {
-
-    if (!isAuthenticated(request)) {
-        const msg = "Access denied"
-        const result = Response.json({ message: msg }, { status: 401, statusText: msg });
-        return result;
-    } else if (!canAccess(request)) {
-        const msg = "Access denied"
-        const result = Response.json({ message: msg }, { status: 403, statusText: msg });
-        return result;
-    }
-
+async function createEntryFilter(request: NextRequest): Promise<EntryFilter<SpellGuideline, string>> {
     var filter: EntryFilter<SpellGuideline, string> = () => true;
     try {
         // Get does not have content
@@ -339,29 +477,100 @@ export async function POST(request: NextRequest) {
             default:
                 // Unknown content type.
                 const msg = `Invalid request content type`;
-                return NextResponse.json({ message: msg }, { status: 400 });
+                return Promise.reject(new ErrorResponse({ message: msg, errorCode: "400" }));
         }
     } catch (error) {
         const message = (typeof error === "string" ? error :
-            (error instanceof Error ? error.message : "Invalid request"))
-        return NextResponse.json({ message: message }, { status: 400 });
+            (error instanceof Error ? error.message : "Invalid request"));
+        return Promise.reject(new ErrorResponse({ message, errorCode: "400" }));
+    }
+    return filter;
+}
+
+/**
+ * Create a new guideline.
+ * @param request 
+ */
+export async function POST(request: NextRequest) {
+
+    if (!isAuthenticated(request)) {
+        const msg = "Access denied"
+        const errorMsg = new ErrorResponse({ message: msg, errorCode: "401" });
+        const result = Response.json(errorMsg);
+        //const result = Response.json({ message: msg }, { status: 401, statusText: msg });
+        return result;
+    } else if (!canAccess(request)) {
+        const msg = "Access denied"
+        //const result = Response.json({ message: msg }, { status: 403, statusText: msg });
+        const result = Response.json(new ErrorResponse({ message: msg, errorCode: "403" }));
+        return result;
     }
 
-    const result: Identified<SpellGuideline>[] = [];
-    for (const [key, guideline] of guidelines.entries()) {
-        if (filter(key, guideline)) {
-            result.push(createIdentified(key, guideline));
+    // Parsing the body.
+    try {
+        /**
+         * @todo: Replace with call to acquire a guid.
+         */
+        const guid = GUID.createV4();
+
+        const guideline: SpellGuideline = await request.json().then(
+            source => (parseJsonGuideline(source))
+        );
+        guidelines.set(guid.toString(), guideline);
+        return Response.json(guid);
+    } catch (err) {
+        if (err instanceof ErrorResponse) {
+            return Response.json(ErrorResponse, { status: Number(err.errorCode) })
+        }
+        return Response.json(new ErrorResponse({ message: "Invalid guideline", errorCode: "400" }), { status: 400 });
+    }
+}
+
+/**
+ * Get filtered guideline entries.
+ * @param request The request.
+ * @returns The promise of Identified entries passing the filter.
+ */
+export async function getFilteredGuidelines(request: NextRequest) {
+    if (!isAuthenticated(request)) {
+        const msg = "Access denied"
+        const errorMsg = new ErrorResponse({ message: msg, errorCode: "401" });
+        const result = Response.json(errorMsg);
+        //const result = Response.json({ message: msg }, { status: 401, statusText: msg });
+        return result;
+    } else if (!canAccess(request)) {
+        const msg = "Access denied"
+        //const result = Response.json({ message: msg }, { status: 403, statusText: msg });
+        const result = Response.json(new ErrorResponse({ message: msg, errorCode: "403" }));
+        return result;
+    }
+
+
+    try {
+        const filter = await createEntryFilter(request);
+
+        const result: Identified<SpellGuideline>[] = [];
+        for (const [key, guideline] of guidelines.entries()) {
+            if (filter(key, guideline)) {
+                result.push(createIdentified(key, guideline));
+            }
+        }
+
+
+        switch (request.headers.get("Accept")) {
+            case "application/json":
+                return NextResponse.json<Identified<SpellGuideline, string>[]>(result);
+            default:
+                return NextResponse.error();
+        }
+    } catch (err) {
+        if (err instanceof ErrorResponse && ["message", "status"].every(prop => prop in err)) {
+            return NextResponse.json(err);
+        } else {
+            throw err;
         }
     }
 
-
-    switch (request.headers.get("Accept")) {
-        case "application/json":
-            return NextResponse.json<Identified<SpellGuideline, string>[]>(result);
-        default:
-            return NextResponse.error();
-    }
-    
 }
 
 /**
@@ -400,24 +609,3 @@ export async function GET(request: NextRequest) {
 
 }
 
-/**
- * Test alteration request acceptance.
- * @param request The request.
- * @returns True, if and only if the user has right to alter the data.
- */
-function isAuthorizedToAlter(request: NextRequest): boolean {
-    const apiKey = request.cookies.get("API-key");
-    return (process.env.DATA_API_KEY !== undefined && process.env.DATA_API_KEY === apiKey?.value);
-}
-
-/**
- * Handling storing spell guidelines.
- * @param request The request. 
- */
-export async function PUT(request: NextRequest) {
-    if (!isAuthorizedToAlter(request)) {
-        const msg = "Access denied"
-        const result = Response.json({ message: msg }, { status: 403, statusText: msg });
-        return result;
-    }
-}
