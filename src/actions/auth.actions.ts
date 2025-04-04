6732fe90c00693679f05a603e9a6dc913a6f5e5e
kautsu2000 <kautsu2000@yahoo.com>
2025-04-04 07:55:05 +0000

    feat: authentication actions
    A module for authentication actions.
    
    On branch auth
    Changes to be committed:
    	new file:   src/actions/auth.actions.ts
        - feat: action singup for creating a new account.
        - feat: aciton login for login an existing account.

---
+++ src/actions/auth.actions.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import { EmailField, PasswordField, validEmail, validPassword } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+
+/**
+ * The actions related to the authentication.
+ */
+export interface ErrorStruct {
+    /**
+     * The error message of an invalid email.
+     */
+    email?: string;
+
+    /**
+     * The error message of an invalid password.
+     */
+    password?: string;
+}
+
+
+/**
+ * Signup action signing up to the system.
+ * @param formData The signup form contents.
+ */
+export async function signup(previousState: ErrorStruct, formData: FormData): Promise<ErrorStruct> {
+
+    const email = formData.get(EmailField);
+    const password = formData.get(PasswordField);
+
+    var errors : ErrorStruct = {
+    };
+    if (!validEmail(email?.toString() ?? "")) {
+        errors[EmailField] = "Please, enter a valid email address";
+    }
+    if (!validPassword(password?.toString() ?? "")) {
+        errors[PasswordField] = "A password must have at least 14 characters."
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return errors;
+    } else {
+        /**
+         * @todo Perform signup.
+         */
+
+        // Redirect. 
+        redirect("/");
+    }
+}
+
+/**
+ * Login action. 
+ * @param formData The login form contents.
+ */
+export async function login(previousState: ErrorStruct, formData:FormData): Promise<ErrorStruct> {
+    const email = formData.get(EmailField);
+    const password = formData.get(PasswordField);
+
+    var errors : ErrorStruct = {
+    };
+
+    if (Object.keys(errors).length > 0) {
+        return errors;
+    } else {
+        /**
+         * @todo Perform creation of a session.
+         */
+
+        // Redirect. 
+        redirect("/");
+    }
+
+}
