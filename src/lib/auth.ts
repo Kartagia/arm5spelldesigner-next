623a0dcc8f88fa9a7c700065425f5bdb9ac67923
kautsu2000 <kautsu2000@yahoo.com>
2025-04-05 22:45:27 +0000

    module: authentication, db and users
    Three modules used for authentication:
    - module auth handles authentication.
    - module db handles database handle pooling for both authentication and
    api database access using postgresql module "pg" for node.
    - module users handles user information operation and user creation,
    updating, and deleting.
    Changes to be committed:
    	modified:   src/lib/auth.ts
        - feat: interface AutHConfig for authentication configuration.
        - feat: error class PasswordCompromisedError
        - feat: function validHashedPassword to perform security checks
        for the hashed passwords.
          - TODO: implement Copenhagen protocol check whether the hash
            has been leaked.
        - feat: function generateSalt for getting a new salt.
        - feat: function hashPassword for hashing a password with a salt.
        - feat: function setPassword for set user credentials and generate
        a new salt.
        - feat: function createSession to create a new session for user.
        - feat: interface Credentials for credential structure.
    
    	new file:   src/lib/db.ts
        - feat: createAuthConnection() to get authentication pool client.
        - feat: createApiConnection() to get api pool sclient.
        - feat: getAuthDatabaseProperties() to get authentication database
        postgresql pool properties.
        - feat: getApiDatabaseProperties() to get API database
        postgresql pool properties.
        - feat: createTransaction(pool) to get a new pool client with a
        new transaction.
        - feat: authQuery to perform authentication database query.
            - feat: optional parameter transaction to given transaction
            into which the query belongs.
        - feat: apiQuery to perform api database query.
            - feat: optional parameter transaction to given transaction
            into which the query belongs.
    
    	modified:   src/lib/users.ts
        - BREAKING CHANGE: interface UserInfo
          - rename: property guid renamed to id.
        - BREAKIGN CHANGE: function createUser
          - feat: added created user credentials as a parameter.
        - feat: optional property verified whether the email has been
        verified.
        - feat: optional property expired determining if the account has
        been expired.
        - feat: interface UserConfig for user information related
        configuration.
          - feat: property userExpration
            The user account expiration due inactivity in days.
        - feat: function getUserInfo to get user information for an existing
        user.
        - feat: function setUserPassword for setting a user's password.
        - feat: function deleteUser for removing a user from users.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,56 @@
 
 import { NodePostgresAdapter } from "@lucia-auth/adapter-postgresql";
 import { Lucia, TimeSpan } from "lucia";
-import pg from 'pg';
-const pool = new pg.Pool();
+import pg, { Connection, PoolClient } from 'pg';
+import { createApiConnection, getAuthDabaseProperties } from "./db";
+import { randomBytes, pbkdf2 } from 'node:crypto';
+import { UserInfo } from "./users";
+const pool = new pg.Pool(getAuthDabaseProperties());
 const adapter = new NodePostgresAdapter(pool, {
     user: "auth_user",
     session: "user_session"
 });
 
+/**
+ * The authentication related configuration.
+ */
+export interface AuthConfig {
+
+
+    /**
+     * The hashing algorithm iteration count.
+     */
+    iterations: number; 
+
+    /**
+     * The hash key lenght in bytes.
+     */
+    keyLen: number;
+
+    /**
+     * The salt length in bytes.
+     */
+    saltLen: number;
+
+    /**
+     * The hashing digest usest. 
+     */
+    digest: string;
+}
+
+const config : AuthConfig = {
+    iterations: 100000,
+    keyLen: 64,
+    saltLen: 20,
+    digest: 'sha512'
+}
+
+/**
+ * The lucian object used for session management.
+ * 
+ * ## Custom session attributes:
+ * - apiKey of database field api_key: The API key of the session.
+ */
 export const lucia = new Lucia(adapter, {
     sessionExpiresIn: new TimeSpan(1, "d"),
     getSessionAttributes: (attributes) => {
@@ -16,10 +59,16 @@ export const lucia = new Lucia(adapter, {
         }
     },
     sessionCookie: {
-        
+
     }
 });
 
+/**
+ * The dyncamid declaration of the lucia module.
+ * 
+ * ## Custom database session fields:
+ * - api_key => varchar: The API key of the session.
+ */
 declare module "lucia" {
     interface Register {
         Lucia: typeof Lucia;
@@ -31,11 +80,13 @@ declare module "lucia" {
 }
 
 /**
- * The authentication module.
+ * The authentication module form field name for email.
  */
-
 export const EmailField = "email";
 
+/**
+ * The authetnicaiton module form field name for password.
+ */
 export const PasswordField = "password";
 
 
@@ -50,10 +101,140 @@ export function validEmail(value: string): boolean {
 }
 
 /**
+ * An error indicating that the password has been compromised into the dark web.
+ */
+export class PasswordCompromisedError extends Error {
+
+    constructor(message: string = "Password hash found in the compromised hashes", 
+        hash: string | undefined = undefined
+    ) {
+        super(message, {cause: hash});
+        this.name = this.constructor.name;
+    }
+}
+
+/**
+ * Test hasshed password validity.
+ * @param hashed The tested hashed password.
+ * @returns Promise of boolean indicating whether the hashed password 
+ * is a valid password.
+ * @throws {PasswordCompromisedError} The password hash has been compromised.
+ */
+export function validHashedPassword(hashed: string): Promise<boolean> {
+
+    /**
+     * @todo Add Copenhagen protocol tests for the hashed password. 
+     */
+
+    // The default test ensures the hashed password is long enough.
+    return Promise.resolve(hashed.length > (128 / 16));
+}
+
+/**
+ * Generate a new salt.
+ * @returns The promise of the generated salt.
+ */
+export function generateSalt(): Promise<string> {
+    var salt: Int8Array = new Int8Array(config.saltLen);
+    return Promise.resolve(randomBytes(config.saltLen).toString('hex'));
+}
+
+/**
+ * Hash a password.
+ * @param password The hashed password.
+ * @param salt The salt.
+ * @returns The hashed passwrod.
+ */
+export function hashPassword(password: string, salt: string): Promise<string> {
+
+    return new Promise(async (resolve, reject) => {
+        pbkdf2(password, salt, config.iterations, config.keyLen, config.digest, (
+            err, derivedKey
+        ) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(derivedKey.toString('hex'));
+            }
+        })
+    });
+}
+
+/**
  * Test validity of a password.
  * @param value The tested value.
  * @returns True, if and only if the email is a valid password.
  */
 export function validPassword(value: string): boolean {
     return value.trim().length >= 14;
+}
+
+/**
+ * Changes the password of the user and update credentials.
+ * @param userId The user identifier.
+ * @param password The password. 
+ * @param transaction The database transaction into which the operation belongs.
+ * Defaults to a new autocommited transaction.
+ * @returns The promise of completion.
+ */
+export async function setPassword(userId: string, password: string,
+    transaction: PoolClient | undefined = undefined): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+        const dbh = transaction ?? (await createApiConnection().then(
+            async conn => {await conn.connect(); return conn},
+            error => { throw error }));
+        try {
+            if (!transaction) {
+                await dbh.query("start transaction");
+            }
+            const salt = await generateSalt();
+            await dbh.query(
+                "insert into user_credentials (id, password, salt) "+
+                "VALUES ($1, $2, $3)", 
+                [userId, await hashPassword(password, salt), salt]
+            );
+            if (!transaction) {
+                await dbh.query("commit");
+            }
+        } catch (error) {
+            // Error handling.
+            console.error("setPassword: Coudl not update user credentials", error);
+            throw error;
+        } finally {
+            if (!transaction) {
+                dbh.release();
+            }
+        }
+    });
+}
+
+
+/**
+ * Create a new API session.
+ * @param userId The user id.
+ * @param apiKey The api key.
+ * @returns The promise of session.
+ */
+export function createSession(userId: string, apiKey: string) {
+    return lucia.createSession(userId, { api_key: apiKey });
+}
+/**
+ * The credentials of the user.
+ */
+
+export interface Credentials {
+    /**
+     * The user id.
+     */
+    id: string;
+
+    /**
+     * The salt of the usr password.
+     */
+    salt: string;
+
+    /**
+     * The hashed salted password.
+     */
+    password: string;
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,134 @@
+"use server";
+import pg, { Connection, Pool, PoolClient, PoolOptions, QueryResult, QueryResultRow } from 'pg';
+
+/**
+ * The database connection module.
+ * @module db
+ */
+
+/**
+ * Get the pool options for the authentication database.
+ * @returns The pool options for authentication database.
+ */
+export function getAuthDabaseProperties(): Partial<PoolOptions> {
+
+    return {
+        database: process.env.AUTH_DATABASE,
+        user: process.env.AUTH_USER,
+        host: process.env.AUTH_HOST,
+        port: Number(process.env.AUTH_PORT),
+        password: process.env.AUTH_PASSWORD,
+    };
+}
+
+/**
+ * The connection pool for accessing authentication.
+ */
+const authPool = new pg.Pool(getAuthDabaseProperties());
+
+/**
+ * Create authentication session.
+ * @eturns The authentication connection.
+ */
+export function createAuthConnection() {
+    return authPool.connect();
+}
+
+/**
+ * Get the pool options for the api database.
+ * @returns The pool options for api database.
+ */
+export function getApiDatabaseProperties(): Partial<PoolOptions> {
+    return {
+        database: process.env.DATA_DATABASE,
+        user: process.env.DATA_USER,
+        host: process.env.DATA_HOST,
+        port: Number(process.env.DATA_PORT),
+        password: process.env.DATA_PASSWORD,
+    }; 
+}
+
+/**
+ * The database pool accessing the API data.
+ */
+const apiPool = new pg.Pool(getApiDatabaseProperties());
+
+/**
+ * Get the API connection.
+ * @returns The API connection.
+ */
+export function createApiConnection() {
+    return apiPool.connect();
+}
+
+
+/**
+ * Create a new transaction.
+ * @param pool The connection pool.
+ * @returns The promise of a connection handling the transaction.
+ */
+export async function createTransaction(pool: Pool): Promise<PoolClient> {
+    return await pool.connect().then( async (connection) => {
+        await connection.query("start transaction");
+        return connection;
+    });
+}
+
+/**
+ * Perform an api query. 
+ * @param sql The SQL of the query.
+ * @param params The parameters of the query.
+ * @param transaction The optional transaction into which the query belongs.
+ * Defaults to a new autocommitting transaction.
+ * @returns The promise of the query result.
+ */
+export function apiQuery<RESULT extends QueryResultRow = any>(sql: string, params: any[], transaction: pg.PoolClient|undefined = undefined) : Promise<QueryResult<RESULT>> {
+    return new Promise( async (resolve, reject) => {
+        const dbh = transaction ?? ( await createTransaction(apiPool) );
+        try {
+            const result : pg.QueryResult<RESULT> = await dbh.query(sql, params);
+            if (!transaction) {
+                await dbh.query("commit");
+                dbh.release();
+            }
+            resolve(result);
+        } catch(error) {
+            await dbh.query("rollback");
+            if (!transaction) {
+                dbh.release();
+            }
+            reject(error);
+        }
+
+    })
+}
+
+/**
+ * Perform an authentication query. 
+ * @param sql The SQL of the query.
+ * @param params The parameters of the query.
+ * @param transaction The optional transaction into which the query belongs.
+ * Defaults to a new autocommitting transaction.
+ * @returns The promise of the query result.
+ */
+export function authQuery<RESULT extends QueryResultRow>(sql: string, params: any[], transaction: pg.PoolClient|undefined = undefined) {
+    return new Promise( async (resolve, reject) => {
+        const dbh = transaction ?? ( await createTransaction(authPool) );
+        try {
+            const result : pg.QueryResult<RESULT> = await dbh.query(sql, params);
+            if (!transaction) {
+                await dbh.query("commit");
+            }
+            resolve(result);
+        } catch(error) {
+            await dbh.query("rollback");
+            reject(error);
+        } finally {
+            if (!transaction) {
+                dbh.release();
+            }
+        }
+
+    })
+
+}
