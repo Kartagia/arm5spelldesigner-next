847f423ae992774bba6ec734d2075769144a83e4
kautsu2000 <kautsu2000@yahoo.com>
2025-04-06 21:38:30 +0000

    feat: authentication actions update and page
    Changes to be committed:
    	modified:   src/actions/auth.actions.ts
        - refactor: use the field name constants in structures.
        - feat: interface LoginFormStatus
    	new file:   src/app/login/page.tsx
        - feat: login page asking username and password.

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
-import { EmailField, PasswordField, validEmail, validPassword } from "@/lib/auth";
+import { createSession, EmailField, generateSalt, hashPassword, PasswordCompromisedError, PasswordField, validEmail, validHashedPassword, validPassword } from "@/lib/auth";
+import { createAuthConnection } from "@/lib/db";
+import { createUser, seekUserInfo, setUserPassword } from "@/lib/users";
 import { redirect } from "next/navigation";
 
 
@@ -11,12 +13,12 @@ export interface ErrorStruct {
     /**
      * The error message of an invalid email.
      */
-    email?: string;
+    [EmailField]?: string;
 
     /**
      * The error message of an invalid password.
      */
-    password?: string;
+    [PasswordField]?: string;
 }
 
 
@@ -24,26 +26,55 @@ export interface ErrorStruct {
  * Signup action signing up to the system.
  * @param formData The signup form contents.
  */
-export async function signup(previousState:  {errors?: ErrorStruct, origin?:string}, formData: FormData) {
+export async function signup(previousState: LoginFormState, formData: FormData) {
 
-    const email = formData.get(EmailField);
-    const password = formData.get(PasswordField);
+    const email = formData.get(EmailField)?.toString() ?? "";
+    const password = formData.get(PasswordField)?.toString() ?? "";
 
-    var errors : ErrorStruct = {
+    var errors: ErrorStruct = {
     };
-    if (!validEmail(email?.toString() ?? "")) {
+    if (!validEmail(email)) {
         errors[EmailField] = "Please, enter a valid email address";
     }
-    if (!validPassword(password?.toString() ?? "")) {
-        errors[PasswordField] = "A password must have at least 14 characters."
+    if (!validPassword(password)) {
+        errors[PasswordField] = "Please, enter a password with at least 15 characters."
     }
+    // Generate the password and salted hash.
+    const salt = await generateSalt();
+    const hashed = await hashPassword(password, salt);
+    validHashedPassword(hashed).then(
+        (isValid) => {
+            if (!isValid) {
+                errors[PasswordField] = "Please, enter a password with at least 15 characters."
+            }
+        },
+        (error) => {
+            errors[PasswordField] = "The password has been compromised. Plase, choose another password.";
+        }
+    )
+
+
 
     if (Object.keys(errors).length > 0) {
-        return {...previousState, errors};
+        return { ...previousState, errors };
     } else {
         /**
          * @todo Perform signup.
          */
+        await createUser({ email }, { password }).then(
+            (userId) => {
+                console.log(`User ID[${userId}] reserved for email[${email}]`);
+                // Create session.
+                createSession(userId);
+            },
+            error => {
+                return {
+                    ...previousState, errors: {
+                        [EmailField]: "Please, choose another email address."
+                    }
+                };
+            }
+        )
 
         // Redirect. 
         redirect("/");
@@ -54,15 +85,49 @@ export async function signup(previousState:  {errors?: ErrorStruct, origin?:stri
  * Login action. 
  * @param formData The login form contents.
  */
-export async function login(previousState: {errors?: ErrorStruct, origin?:string}, formData:FormData) {
-    const email = formData.get(EmailField);
-    const password = formData.get(PasswordField);
+export async function login(previousState: LoginFormState, formData: FormData) {
+    const email = formData.get(EmailField)?.toString() ?? "";
+    const password = formData.get(PasswordField)?.toString() ?? "";
 
-    var errors : ErrorStruct = {
+    var errors: ErrorStruct = {
     };
 
+    if (!email) {
+        // The email missing.
+        errors[EmailField] = "Please, enter an email address.";
+    } else if (!validEmail(email)) {
+        // The email address was not a valid email addrss.
+        errors[EmailField] = "Please, enter a valid email address.";
+    } else {
+
+        const userId = await seekUserInfo({ email }).then(
+            (userInfo) => {
+                return userInfo.id;
+            },
+            (error) => {
+                errors[EmailField] = "Invalid user name or password";
+                errors[PasswordField] = "Invalid user name or password";
+                return undefined;
+            }
+        );
+        if (userId) {
+            await setUserPassword(userId, password).then(
+                () => {
+                    redirect(origin);
+                },
+                (error) => {
+                    if (error instanceof PasswordCompromisedError) {
+                        // This does not prevent login. 
+                        errors[PasswordField] = ""
+                    }
+                    errors[PasswordField] = error;
+                }
+            )
+        }
+    }
+
     if (Object.keys(errors).length > 0) {
-        return {...previousState, errors};
+        return { ...previousState, errors };
     } else {
         /**
          * @todo Perform creation of a session.
@@ -72,4 +137,20 @@ export async function login(previousState: {errors?: ErrorStruct, origin?:string
         redirect(previousState?.origin ?? "/");
     }
 
+}
+/**
+ * The login form state.
+ */
+export interface LoginFormState {
+
+    /**
+     * The origin of the login where the application is redirected on success.
+     * @default "/" The application home.
+     */
+    origin?: string;
+
+    /**
+     * The errors on the form.
+     */
+    errors?: Record<string, string>;
 }
