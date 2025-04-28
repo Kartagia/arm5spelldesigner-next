1e869e521fa97ca769c0322d9ac6dd7f9735d233
Antti Kautiainen <antti@kautiainen.com>
2025-04-28 10:13:33 +0300

    feat: transaction support for users library
    Added transaction support for users library methods.
    
    Changes to be committed:
    	modified:   src/lib/users.ts
        - feat: type NewCredentials for a new user's credentials.
        - feat: type NewUserInfo for a new user's userinfo.
        - feat: createUsers to create several users as a transaction.
        - feat: createUsers transaction support.
        - feat: setUserPassword transaction support.
        - feat: updateUser transaction support.
        - feat: deleteUser transaction support.

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -21,7 +21,7 @@ export interface UserConfig {
     userExpiration: number;
 }
 
-const config : UserConfig = {
+const config: UserConfig = {
     userExpiration: 90
 }
 
@@ -62,11 +62,11 @@ export async function loginUser(email: string, password: string): Promise<UserIn
         return Promise.reject(new SyntaxError("Invalid email address"));
     }
 
-    return await createAuthConnection().then( 
+    return await createAuthConnection().then(
         async (dbh) => {
             return dbh.query<UserInfo>("SELECT id, email, displayName, verified, expires FROM auth_user  WHERE email=$1 AND expires > NOW()",
                 [email]
-            ).then( async (result) => {
+            ).then(async (result) => {
                 if (result.rowCount === 0) {
                     // Not found.
                     throw new Error("Not Found");
@@ -74,21 +74,21 @@ export async function loginUser(email: string, password: string): Promise<UserIn
                     // Validating the user. 
                     const userInfo = result.rows[0];
                     return dbh.query<Credentials>("SELECT password, salt FROM user_credentials WHERE id=$1", [userInfo.id])
-                    .then( 
-                        (credentials) => {
-                            return (checkUserPassword(credentials.rows[0].password, password, credentials.rows[0].salt)).then(
-                                (valid) => {
-                                    if (valid) {
-                                        return userInfo;
-                                    } else {
-                                        throw new Error("Forbidden");
+                        .then(
+                            (credentials) => {
+                                return (checkUserPassword(credentials.rows[0].password, password, credentials.rows[0].salt)).then(
+                                    (valid) => {
+                                        if (valid) {
+                                            return userInfo;
+                                        } else {
+                                            throw new Error("Forbidden");
+                                        }
+                                    },
+                                    () => {
+                                        throw new Error("Unaouthorized");
                                     }
-                                }, 
-                                () => {
-                                    throw new Error("Unaouthorized");
-                                }
-                            )
-                    });
+                                )
+                            });
                 }
             })
         }
@@ -96,36 +96,93 @@ export async function loginUser(email: string, password: string): Promise<UserIn
 }
 
 /**
+ * Credentials for a new user.
+ */
+export type NewCredentials = Omit<Credentials, "id" | "salt">;
+
+/**
+ * User information for a new user.
+ */
+export type NewUserInfo = Omit<UserInfo, "id" | "expired">;
+
+/**
  * Create a new user.
  * @param details The user details.
  * @param credentials The user credentials.
+ * @param transaction The transaction used to add user. 
  * @returns The promsie of completion.
  */
-export async function createUser(details: Omit<UserInfo, "id">, credentials: Omit<Credentials, "id" | "salt">): Promise<string> {
+export async function createUser(details: NewUserInfo, credentials: NewCredentials, transaction: PoolClient | undefined = undefined): Promise<string> {
 
     return new Promise(async (resolve, reject) => {
-        const dbh = await createAuthConnection();
-        await dbh.connect();
+
         try {
-            var id = crypto.randomUUID();
-            await dbh.query("start transaction");
-            await dbh.query("insert into auth_user (id, email, displayname, verified, expires) values ($1, $2, $3, $4, NOW() + INTERVAL '$5 DAYS')",
-                [
-                    id, details.email, details.displayName, details.verified ?? false,
-                    Math.max(0, config.userExpiration)
-                ]);
-            await setPassword(id, credentials.password, dbh);
-            await dbh.query("commit");
-            resolve(id);
-        } catch (error) {
-            /**
-             * @todo Error handling returning standardized error as rejection.
-             */
-            throw error;
-        } finally {
-            dbh.release();
+            const dbh = transaction ?? (await createAuthConnection().then(async (dbh) => {
+                await dbh.query("begin");
+                return dbh;
+            }).catch((err) => { throw err }));
+            try {
+                var id = crypto.randomUUID();
+                await dbh.query("insert into auth_user (id, email, displayname, verified, expires) values ($1, $2, $3, $4, NOW() + INTERVAL '$5 DAYS')",
+                    [
+                        id, details.email, details.displayName, details.verified ?? false,
+                        Math.max(0, config.userExpiration)
+                    ]);
+                await setPassword(id, credentials.password, dbh);
+                // Committing the transaction, if we are doing just one creation.
+                if (!transaction) {
+                    await dbh.query("commit");
+                    dbh.release();
+                }
+                resolve(id);
+            } catch (error) {
+                /**
+                 * @todo Error handling returning standardized error as rejection.
+                 */
+                if (!transaction) {
+                    await dbh.query("rollback");
+                    dbh.release();
+                }
+                throw error;
+            }
+        } catch (err) {
+            reject(err);
         }
     });
+}
+
+/**
+ * Create several users.
+ * @param users The users created.
+ * @param transaction The transaction the operation is part of.
+ */
+export async function createUsers(users: [NewUserInfo, NewCredentials][], transaction: PoolClient | undefined = undefined): Promise<void> {
+    const dbh = transaction ?? await createAuthConnection().then(async (dbh) => {
+        await dbh.query("begin");
+        return dbh;
+    });
+    if (dbh) {
+        try {
+            for (const [user, credentials] of users) {
+                await createUser(user, credentials, dbh);
+            }
+
+            // Committing the changes.
+            if (!transaction) {
+                await dbh.query("commit");
+                dbh.release();
+            }
+        } catch (err) {
+            // Rollback on error.
+            if (!transaction) {
+                await dbh.query("rollback");
+                dbh.release();
+            }
+            throw err;
+        }
+    } else {
+        throw new Error("Connection refused.")
+    }
 }
 
 /**
@@ -148,7 +205,7 @@ export async function getUserInfo(userId: string, transaction: PoolClient | unde
                     email: row.email,
                     verified: row.verified ?? false,
                     expired: row.expired ?? true
-                }; 
+                };
                 if (!transaction) {
                     dbh.release();
                 }
@@ -168,16 +225,25 @@ export async function getUserInfo(userId: string, transaction: PoolClient | unde
  * Set user password.
  * @param userId The user identifier.
  * @param password The password.
+ * @param transaction A database handle of the transaction. 
  */
-export async function setUserPassword(userId: string, password: string): Promise<void> {
-    return createAuthConnection().then( async dbh => {
+export async function setUserPassword(userId: string, password: string, transaction: PoolClient|undefined = undefined): Promise<void> {
+    return (transaction ? Promise.resolve(transaction) : createAuthConnection().then(async dbh => { 
+        await dbh.query("begin");
+        return dbh;
+    })).then(async (dbh) => {
         try {
-            await dbh.query("start transaction");
             await setPassword(userId, password, dbh);
-            updateUser({id: userId}, dbh);
-            await dbh.query("commit");
-        } catch( err) {
-            await dbh.query("rollback");
+            await updateUser({ id: userId }, dbh);
+            if (!transaction) {
+                await dbh.query("commit");
+                dbh.release();
+            }
+        } catch (err) {
+            if (!transaction) {
+                await dbh.query("rollback");
+                dbh.release();
+            }
             throw err;
         }
     });
@@ -189,49 +255,49 @@ export async function setUserPassword(userId: string, password: string): Promise
  * @param transaction Teh transaction used instead of new connection.
  * @returns The promise of completion. 
  */
-export async function updateUser(details: Partial<Omit<UserInfo, "expired">>, transaction: PoolClient|undefined = undefined): Promise<void> {
+export async function updateUser(details: Partial<Omit<UserInfo, "expired">>, transaction: PoolClient | undefined = undefined): Promise<void> {
     if (details.id == undefined) {
         return Promise.reject(new Error(`User identifier is required for update`));
     } else if (Object.keys(details).length === 1) {
         // Updating expiration only. 
-        return (transaction ? Promise.resolve(transaction) : 
-        createAuthConnection().then( async (dbh) => {
-            await dbh.query("begin");
-            return dbh;
-        })).then(
-            dbh => {
-                dbh.query("UPDATE auth_user SET expires = NOW + INTERVAL '$1 DAYS' WHERE id=$2", [Math.max(0, config.userExpiration), details.id]);
-            }
-        );
-    } else {
-        return new Promise(async (resolve, reject) => {
-            (transaction ? Promise.resolve(transaction) : 
-            createAuthConnection().then( async (dbh) => {
+        return (transaction ? Promise.resolve(transaction) :
+            createAuthConnection().then(async (dbh) => {
                 await dbh.query("begin");
                 return dbh;
             })).then(
-                async dbh => {
-                    const [fields, values]: [(keyof typeof details)[], any[]] = (Object.keys(details) as (keyof typeof details)[]).reduce( 
-                        (result: [(keyof typeof details)[], any[]], key: keyof typeof details, index:number) => {
-                        if (key !== 'id' && key in details) {
-                            result[0].push(key);
-                            result[1].push(details[key]);
+                dbh => {
+                    dbh.query("UPDATE auth_user SET expires = NOW + INTERVAL '$1 DAYS' WHERE id=$2", [Math.max(0, config.userExpiration), details.id]);
+                }
+            );
+    } else {
+        return new Promise(async (resolve, reject) => {
+            (transaction ? Promise.resolve(transaction) :
+                createAuthConnection().then(async (dbh) => {
+                    await dbh.query("begin");
+                    return dbh;
+                })).then(
+                    async dbh => {
+                        const [fields, values]: [(keyof typeof details)[], any[]] = (Object.keys(details) as (keyof typeof details)[]).reduce(
+                            (result: [(keyof typeof details)[], any[]], key: keyof typeof details, index: number) => {
+                                if (key !== 'id' && key in details) {
+                                    result[0].push(key);
+                                    result[1].push(details[key]);
+                                }
+                                return result;
+                            }, [[], []] as [(keyof typeof details)[], any[]]);
+                        const result = dbh.query(`UPDATE auth_user SET expires = NOW() + INTERVAL '$1 days' AND ${fields.map(
+                            (field: keyof UserInfo, index: number) => (`${escapeIdentifier(field)}=$${index + 2}`)
+                        ).join(" AND ")} WHERE id=$${fields.length + 2}`, [
+                            Math.max(0, config.userExpiration), ...values, details.id
+                        ]);
+                        if (!transaction) {
+                            // Closing transaction.
+                            await dbh.query("commit");
+                            dbh.release();
                         }
                         return result;
-                    }, [[], []] as [(keyof typeof details)[], any[]]);
-                    const result = dbh.query(`UPDATE auth_user SET expires = NOW() + INTERVAL '$1 days' AND ${ fields.map( 
-                        (field: keyof UserInfo, index: number) => (`${escapeIdentifier(field)}=$${index+2}`) 
-                    ).join(" AND ")} WHERE id=$${fields.length+2}`, [
-                        Math.max(0, config.userExpiration), ...values, details.id
-                    ]);
-                    if (!transaction) {
-                        // Closing transaction.
-                        await dbh.query("commit");
-                        dbh.release();
                     }
-                    return result;
-                }
-            )
+                )
         });
     }
 }
@@ -241,23 +307,25 @@ export async function updateUser(details: Partial<Omit<UserInfo, "expired">>, tr
  * @param details The user details.
  * @returns The promise of completion.
  */
-export async function deleteUser(details: UserInfo, transaction: PoolClient|undefined = undefined): Promise<void> {
+export async function deleteUser(details: UserInfo, transaction: PoolClient | undefined = undefined): Promise<void> {
     return new Promise(async (resolve, reject) => {
         const dbh = transaction ?? await createAuthConnection();
         if (!transaction) {
             await dbh.query("begin");
         }
-        const userInfo = await getUserInfo(details.id, dbh);
-        return await createAuthConnection().then(
-            (dbh) => {
-                return dbh.query("DELETE FROM auth_user WHERE id=$1", [details.id]);
-            }
-        ).then( (result) => {
+        try {
+            const userInfo = await getUserInfo(details.id, dbh);
+            await dbh.query("DELETE FROM auth_user WHERE id=$1", [userInfo.id]);
             if (!transaction) {
-                dbh.query("commit");
+                await dbh.query("commit");
                 dbh.release();
             }
-            return result;
-        })
+        } catch (err) {
+            if (!transaction) {
+                await dbh.query("rollback");
+                dbh.release();
+            }
+            reject(err);
+        }
     });
 }
