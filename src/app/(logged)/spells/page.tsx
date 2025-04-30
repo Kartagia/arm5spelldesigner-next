9d12cccfe0864139f81cd9b7a4713b21a12c5765
Antti Kautiainen <antti@kautiainen.com>
2025-04-30 09:54:02 +0300

    page: spell editor page
    The page opening the spell editor component and performing
    server side laoding of the data for the react client component.
    
    	new file:   src/app/(logged)/spells/page.tsx
        - The spell designer page.

---
+++ src/app/(logged)/spells/page.tsx
@@ -0,0 +1,41 @@
+import { getAllGuidelines, getAllSpells } from "@/lib/spells";
+import { getAllRDTs } from "@/data/rdts";
+import { getAllArts, getAllForms, getAllTechniques } from "@/data/arts";
+import { Suspense } from "react";
+import { SpellDesignerPanel } from "@/components/SpellDesignerPanel";
+
+
+async function SpellDesignerWrapper() {
+    const forms = await getAllForms();
+    const techniques = await getAllTechniques();
+    const guidelines = await getAllGuidelines();
+    const allRDTs = await getAllRDTs();
+    const spells = await getAllSpells();
+
+    console.table({
+        forms, techniques
+    })
+
+    return (<SpellDesignerPanel spells={spells} rdts={[]} forms={forms} techniques={techniques} guidelines={guidelines}/>)
+}
+
+
+
+/**
+ * Spell editor page.
+ */
+export default function SpellsPage() {
+
+
+    return (
+        <section className="primary">
+            <header className="title primary">Spells</header>
+            <main className="main">
+                <Suspense fallback="Loading...">
+                    <SpellDesignerWrapper />
+                </Suspense>
+            </main>
+            <footer className="footer">Spell designer blurb goes here.</footer>
+        </section>
+    )
+}
