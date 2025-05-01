6272f1158ab4405c84ac8e4af0c6be094ee7f92c
Antti Kautiainen <antti@kautiainen.com>
2025-05-01 20:38:28 +0300

    action storeSpells revalidates the layout cache.
    Revalidate layout cache when spells are stored.
    Changes to be committed:
    	modified:   src/actions/spells.actions.ts
        - storing spells revalidate the cached pages.

--- src/actions/spells.actions.ts
+++ src/actions/spells.actions.ts
@@ -3,8 +3,11 @@
 import { storeSpells } from "@/data/spells";
 import { SpellModel } from "@/lib/spells";
 import { UUID } from "crypto";
+import { revalidatePath } from "next/cache";
 
 export async function saveSpells( spells: SpellModel[], altered?: UUID[]) {
     console.log("Altered UUIDS: %s", altered ? "[" + altered.join(", ") + "]" : "Not given");
-    storeSpells(spells, altered);
+    await storeSpells(spells, altered);
+
+    revalidatePath("/", "layout");
 }
