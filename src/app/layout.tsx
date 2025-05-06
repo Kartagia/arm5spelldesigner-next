b40cd97e7c1e273bef41b7966a74496d62099f51
Antti Kautiainen <antti@kautiainen.com>
2025-05-06 12:28:56 +0300

    feat: moved login and logout
    Moved the authentication buttons to the footer of the layout
    page of the secured pages section.
    
    Changes to be committed:
    	modified:   src/app/layout.tsx
        - feat: authentication duing layout.
        - feat: Logout button for logged in users on footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 import { fstat } from "fs";
 import { Suspense } from "react";
 import Image from "next/image";
+import { validateSession } from "@/lib/session";
+import { cookies } from "next/headers";
+import LogoutButton from "@/components/LogoutButton";
+import Link from "next/link";
 
 const caudexSans = Caudex({
   weight: "400",
@@ -29,11 +33,14 @@ export const metadata : Metadata = {
   ...(await getDictionary("en-US"))
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const {userInfo} = await validateSession( (await cookies()).get("auth_session")?.value ?? "");
+
+
   return (
     <html lang="en">
       <body
@@ -41,7 +48,9 @@ export default function RootLayout({
       >
         <header className="header"><h1 className="title"><Suspense>{metadata.title?.toString()}</Suspense></h1></header>
         <main className="main flex">{children}</main>
-        <footer className="footer"><Image src="/next.svg" alt="NextJS logo" width={200} height={60}/></footer>
+        <footer className="footer"><Image src="/next.svg" alt="NextJS logo" width={200} height={60}/>
+        {userInfo ? <LogoutButton>Logout</LogoutButton> : <Link href="/login" className="button">Login</Link>}
+        </footer>
       </body>
     </html>
   );
