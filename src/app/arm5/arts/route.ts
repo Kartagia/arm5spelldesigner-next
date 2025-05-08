e5f90cbf65f54695895eb2c86dc1099399941f92
Antti Kautiainen <antti@kautiainen.com>
2025-05-08 11:44:32 +0300

    fix: route /arm5/arts
    The arts route now return spells properly.
    
    Changes to be committed:
    	modified:   src/app/arm5/arts/route.ts
        - fix: The route did not return response.
        - fix: The route did not release the database handle.

--- src/app/arm5/arts/route.ts
+++ src/app/arm5/arts/route.ts
@@ -4,139 +4,76 @@
  * Arts api route. 
  */
 
-import { logger, validateApiRequest } from "@/lib/api_auth";
-import { createApiConnection } from "@/lib/db";
-import { getApiDatabaseProperties } from "@/lib/dbConfig";
-import { UUID } from "crypto";
+import { logger, validateApiRequest, createApiSession } from "@/lib/api_auth";
+import { ApiArt, ErrorReply, JournalId, UnauthorizedReply } from "@/lib/api_data";
+import { safeRelease } from "@/lib/api_db";
+import { createApiConnection, initApiPool } from "@/lib/db";
 import { NextRequest } from "next/server";
-import { Client, Pool } from "pg";
 import { Logger } from "pino";
-import { permission } from "process";
-import { resourceLimits } from "worker_threads";
-
-/**
- * Create GUID reply
- * @param guid
- * @returns 
- */
-function CreateGuidReply(guid: UUID) {
-    return Response.json(guid);
-}
-
-/**
- * Resource content. 
- */
-interface ResourceContent<TYPE> {
-    guid: UUID;
-    value: TYPE;
-}
-
-interface GuidContent {
-    guid: UUID;
-
-}
-
-/**
- * Error content.
- */
-interface ErrorContent {
-    /**
-     * The message of the error.
-     */
-    message?: string;
-
-    /**
-     * The error code. 
-     */
-    errorCode?: number;
-}
-
-interface InvalidResourceContent extends ErrorContent {
-    invalidProrties: { propertyName: string, message?: string }[]
-}
-
-function InvalidResourceReply(invalidProperties: Record<string, string | string[]>, guid?: UUID) {
-
-    return
-}
-
-function ErrorReply(errorCode: number = 400, message?: string) {
-    if (errorCode < 400) {
-        throw new SyntaxError("The result is not an error");
-    }
-    return Response.json({ message, errorCode }, { status: errorCode });
-}
-
-function NotFoundReply(guid?: UUID, refType?: string, message: string = "Not found!") {
-
-    return Response.json({ guid, refType, message, errorCode: 404 }, { status: 404 })
-}
-
-function UnauthorizedReply(message?: string, errorCode: 401 | 403 = 401) {
-    return Response.json({ message: "Access denied. Please login to acquire credentials.", errorCode });
-}
-
-interface RefType<TYPE extends string> {
-    guid: UUID,
-    refType: string,
-    startTime: string
-}
-
-interface ApiArt {
-    guid?: UUID,
-    type: "Form" | "Technique",
-    abbrev: string,
-    name: string,
-    style: string
-}
-
 
 /**
  * Get all arts. 
- * @param request 
- * @returns 
+ * @param request The request to get all arts.
+ * @returns If the user has permissions to read arts, returns the array of guid-art-pairs of the api.
+ * - If user does not have permission, either unauthorized or forbidden response is returend depending
+ * whether the user is not autenticated (unauthorised), or have no rigth toaccess (frobidden).
  */
 export async function GET(request: NextRequest) {
 
     const permissions = await validateApiRequest(request);
     if (permissions.apiKey || permissions.cookieKey) {
         console.log("Permission granted");
-        const result = await createApiConnection().then(
-            (dbh) => {
-                // Database connection available.
-                console.log("Connection established");
-                dbh.query<ApiArt & RefType<"art.form">>("SELECT * FROM api_art_view").then(
-                    async (result) => {
-                        console.log("");
-                        const rows = await result.rows.map(entry => [entry.guid, entry as ApiArt]);
-                        dbh.release();
-                        return Response.json(rows);
-                    },
-                    (error) => {
-                        // Trying without view. 
-                        return dbh.query("SELECT name, artview.type, style, guid, rdt_type, starttime as start_time " + 
-                            "FROM (" + 
-                            "select *, 'Form' as type, 'art.form' as rdt_type from arts join forms as A on arts.id = A.art_id JOIN magicstyles ON style_id = magicstyles.id " +
-                            "UNION " +
-                            "select *, 'Technique' as type, 'art.technique' as rdt_type from arts join techniques as A on arts.id = A.art_id JOIN magicstyles ON style_id = magicstyles.id" +
-                            ") as artsview JOIN guis ON art_id = guids.id AND rdt_type = guids.type"
-                        ).then( 
-                            (result) => {
-                                return Response.json(result.rows.map( row => ([row.guid, {type: row.type, name: row.name, style: row.style}])));
-                            },
-                            (error) => {
-                                logger.error(error, "Fetching rows failed");
-                                return ErrorReply(500, "Database access error");        
-                            }
-                        )
-                    }
-                )
-            },
-            (error) => {
-                logger.error(error, "Connection refused");
-                return ErrorReply(503, "Service is not available");
-            }
-        );
+        try {
+            const result = await createApiConnection().catch( (err) => {
+                if (err === "Not initialized" || "message" in err &&  err.message === "Not initialized" ) {
+                    return initApiPool().then( (pool) => (pool.connect()));
+                } else {
+                    throw err;
+                }
+            }).then(
+                (dbh) => {
+                    // Database connection available.
+                    console.log("Connection established");
+                    return dbh.query<ApiArt & JournalId<"art.form" | "art.technique">>("SELECT * FROM api_arts").then(
+                        async (result) => {
+                            console.log("");
+                            const rows = await result.rows.map(entry => [entry.guid, entry as ApiArt]);
+                            safeRelease(dbh);
+                            return Response.json(rows);
+                        },
+                        (error) => {
+                            // Trying without view. 
+                            return dbh.query<ApiArt & JournalId<"art.form" | "art.technique">>("SELECT name, artview.type, style, guid, rdt_type, starttime as start_time " +
+                                "FROM (" +
+                                "select *, 'Form' as type, 'art.form' as rdt_type from arts join forms as A on arts.id = A.art_id JOIN magicstyles ON style_id = magicstyles.id " +
+                                "UNION " +
+                                "select *, 'Technique' as type, 'art.technique' as rdt_type from arts join techniques as A on arts.id = A.art_id JOIN magicstyles ON style_id = magicstyles.id" +
+                                ") as artsview JOIN guis ON art_id = guids.id AND rdt_type = guids.type"
+                            ).then(
+                                (result) => {
+                                    safeRelease(dbh);
+                                    return Response.json(result.rows.map(row => ([row.guid, { type: row.type, name: row.name, style: row.style }])));
+                                },
+                                (error) => {
+                                    logger.error(error, "Fetching rows failed");
+                                    safeRelease(dbh);
+                                    return ErrorReply(500, "Database access error");
+                                }
+                            )
+                        }
+                    )
+                },
+                (error) => {
+                    logger.error(error, "Connection refused");
+                    return ErrorReply(503, "Service is not available");
+                }
+            );
+            // Release dbh.
+            return result;
+        } catch (error) {
+            logger.error(error, "Fetching data failed");
+            return ErrorReply(500, "Something went wrong.");
+        }
     } else {
         logger.error("Unauthorized access");
         return UnauthorizedReply();
