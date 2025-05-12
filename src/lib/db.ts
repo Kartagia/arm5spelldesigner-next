54d015dd24828cc9eeee18ea4c45ca60c06fd538
Antti Kautiainen <antti@kautiainen.com>
2025-05-12 11:02:45 +0300

    feat: savepoint support for transactions
    Added savepoint support for transactions.
    
    Changes to be committed:
    	modified:   src/lib/db.ts
        - feat: interface TransactionOptions
          - feat: optional feature savePoint contains the save-point name.
        - feat: saveTransaction to create a savepoint returning the
        name of the created savepoint.
        - feat: releaseSavepoint releases savepoint.
        - feat: createNewSavepointName creates a new savepoint name.
        - feat: rollback rolls back to the savepoint, if the options
        contains savepoint.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1045,7 +1045,7 @@ export async function deleteApiDb(dbh: PoolClient | Client, {
  * @returns The transaction identifier.
  */
 export function createTransactionId(): string {
-    return Math.floor(Math.random()*Number.MAX_SAFE_INTEGER).toString(36);
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(36);
 }
 
 /**
@@ -1066,10 +1066,22 @@ export interface TransactionOptions {
     /**
      * The logger used to log errors. 
      */
-    logger?: pino.BaseLogger|Console;
+    logger?: pino.BaseLogger | Console;
+
+    /**
+     * The save point name used for roll backs and commits.
+     */
+    savePoint?: string;
 }
 
-export async function startTransaction(dbh: PoolClient|Client, options: TransactionOptions = {}): Promise<string|undefined> {
+/**
+ * Start a new transaction.
+ * @param dbh The database conneciton.
+ * @param options THe options of the transaction.
+ * @returns The transaction identifier of the created transaction, or an undefined value indicating no
+ * transaction was created.
+ */
+export async function startTransaction(dbh: PoolClient | Client, options: TransactionOptions = {}): Promise<string | undefined> {
     const id = options.id ?? createTransactionId();
     return dbh.query("BEGIN").then(
         () => {
@@ -1080,16 +1092,62 @@ export async function startTransaction(dbh: PoolClient|Client, options: Transact
             (options.logger ?? console).error("Trasaction creation failed. %s", error);
             return undefined;
         }
-    )   
+    )
 }
 
 /**
- * Commit a transaction. 
+ * Create a new savepoint name for transaction.
+ * @param transactionId The tranasction identifier.
+ * @param maxLen The maximum lenght of the name. Defaults to the default Postgresql maximum
+ * identifier size.
+ * @returns The savepoint name for a new savepoint. 
+ */
+export function createNewSavepointName(transactionId: string, maxLen: number = 63) {
+    if (transactionId.length >= maxLen) {
+        // Using transaciton identifier, as it fills the namespace.
+        return transactionId;
+    } else {
+        // Create new savepoint identfier. 
+        const maxBits = maxLen - transactionId.length;
+        return `${transactionId}${Math.floor(Math.random()*Math.min(Number.MAX_SAFE_INTEGER, 2**(maxBits*5))).toString(36)}`
+    }
+}
+
+/**
+ * Start a new transaction savepoint.
+ * @param dbh The database conneciton.
+ * @param options The options of the transaction.
+ * @returns The identifier of the created savepoint, or an undefined value indicating no savepoint was created.
+ */
+export async function saveTransaction(dbh: PoolClient | Client, options: TransactionOptions = {}): Promise<string | undefined> {
+    if (options.id == null) {
+        // There is no transaction. 
+        return undefined;
+    }
+    const savePoint = options.savePoint ?? createNewSavepointName(options.id);
+    if (savePoint) {
+        return dbh.query("SAVEPOINT %s", [savePoint]).then(
+            () => {
+                (options.logger ?? console).debug("Transaction %s savepoint %s created.", options.id, savePoint);
+                return savePoint;
+            },
+            (error) => {
+                (options.logger ?? console).error("Trasaction %s savepoint creation failed. %s", options.id, error);
+                return undefined;    
+            }
+        )
+    } else {
+        return undefined;
+    }
+}
+
+/**
+ * Commit a transaction.
  * @param dbh The database connection.
  * @param options The transaction options.
  * @returns Did the commit happen.
  */
-export async function commitTransaction(dbh: PoolClient|Client, options: TransactionOptions = {}): Promise<boolean> {
+export async function commitTransaction(dbh: PoolClient | Client, options: TransactionOptions = {}): Promise<boolean> {
     const id = options.id ?? "without id";
     return dbh.query("COMMIT").then(
         () => {
@@ -1100,8 +1158,32 @@ export async function commitTransaction(dbh: PoolClient|Client, options: Transac
             (options.logger ?? console).error("Transaction %s commit failed. %s", id, error);
             return false;
         }
-    )   
+    )
+}
 
+/**
+ * Release a transcation to the savepoint of the tranasaction.
+ * @param dbh The database connection.
+ * @param options The transaction options.
+ * @returns Did the release happen.
+ */
+export async function releaseSavepoint(dbh: PoolClient | Client, options: TransactionOptions = {}): Promise<boolean> {
+    if (options.savePoint) {
+        const id = options.id ?? "without id";
+        return dbh.query("RELEASE SAVEPOINT %s", [options.savePoint]).then(
+            () => {
+                (options.logger ?? console).debug("Transaction %s savepoint %s released.", id, options.savePoint);
+                return true;
+            },
+            (error) => {
+                (options.logger ?? console).error("Transaction %s savepoint %s release failed. %s", id, options.savePoint, error);
+                return false;
+            }    
+        )
+    } else {
+        // No savepoint to release.
+        return true;
+    }
 }
 
 /**
@@ -1110,19 +1192,32 @@ export async function commitTransaction(dbh: PoolClient|Client, options: Transac
  * @param options The transaction options.
  * @returns Did the roll back happen.
  */
-export async function rollbackTransaction(dbh: PoolClient|Client, options: TransactionOptions = {}): Promise<boolean> {
+export async function rollbackTransaction(dbh: PoolClient | Client, options: TransactionOptions = {}): Promise<boolean> {
     const id = options.id ?? "without id";
-    return dbh.query("ROLLBACK").then(
-        () => {
-            (options.logger ?? console).debug("Transaction %s rolled back.", id);
-            return true;
-        },
-        (error) => {
-            (options.logger ?? console).error("Transaction %s roll back failed. %s", id, error);
-            return false;
-        }
-    )   
+    if (options.savePoint) {
+        return dbh.query("ROLLBACK TO SAVEPOINT %s", [options.savePoint]).then(
+            () => {
+                (options.logger ?? console).debug("Transaction %s rolled back to savepoint %s.", id, options.savePoint);
+                return true;
+            },
+            (error) => {
+                (options.logger ?? console).error("Transaction %s roll back to savepoint %s failed. %s", id, options.savePoint, error);
+                return false;
+            }
+        );
 
+    } else {
+        return dbh.query("ROLLBACK").then(
+            () => {
+                (options.logger ?? console).debug("Transaction %s rolled back.", id);
+                return true;
+            },
+            (error) => {
+                (options.logger ?? console).error("Transaction %s roll back failed. %s", id, error);
+                return false;
+            }
+        )
+    }
 }
 
 /**
