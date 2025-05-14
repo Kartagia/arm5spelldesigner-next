7be1923da977df2c785ecf1c75c63007b398394a
Antti Kautiainen <antti@kautiainen.com>
2025-05-14 09:49:07 +0300

    feat: spells store spells to the database
    The spells store spells to the database.
    
    Changes to be committed:
    	modified:   src/data/spells.ts
        - fix: createSpells use proper API key for application.
        - fix: storeDbSpells invalid SQL query replaced with proper
        insert or update.
        - feat: storeDbSpell to store single spell to the database.
        - feat: storeSpells store spells to the database.
          -  Replacing the spell store will keep only spells not stored
          into the spell database, and tries to store all given spells.
          -  TODO: removing spells from database.

--- src/data/spells.ts
+++ src/data/spells.ts
@@ -5,7 +5,7 @@
  */
 
 import { checkUUID, RDT, UUIDSupplier, validUUID } from "@/lib/modifiers";
-import { checkSpell, getAllGuidelines, NewSpellModel, SpellLevel, SpellModel } from "@/lib/spells";
+import { checkSpell, getAllGuidelines, NewSpellModel, SpellLevel, SpellModel, spellSignature } from "@/lib/spells";
 import { randomUUID, UUID } from "crypto";
 import { createRDT, getAllRDTs } from "./rdts";
 import { resourceLimits } from "worker_threads";
@@ -14,19 +14,20 @@ import { logger } from "@/lib/api_auth";
 import { error } from "console";
 import { createApiConnection } from "@/lib/db";
 import { safeRelease } from "@/lib/api_db";
+import { getUserInfo } from "@/lib/users";
+import { commitTransaction, rollbackTransaction, startTransaction, TransactionOptions } from "@/lib/transactions";
+import { DatabaseError } from "pg";
 
 const allRdts = await getAllRDTs();
 
-function getRdt<T extends string>(value: RDT<T> | undefined) {
+/**
+ * Get RDT or generate an empty RDT array.
+ * @param value The single value RDT.
+ * @returns Get RDT value of RDT into spell RDT value. 
+ */
+function getSpellModelRdt<T extends string>(value: RDT<T> | undefined) {
     if (value) {
         return [value]
-    } else {
-        return [];
-    }
-}
-function getRdtUUID<T extends string>(value: RDT<T> | undefined): UUID[] {
-    if (value && validUUID(value.guid)) {
-        return [checkUUID(value.guid)]
     } else {
         return [];
     }
@@ -37,39 +38,43 @@ var spellStore: SpellModel[] = [
     {
         guid: randomUUID(),
         name: "Rock of Viscid Clay", technique: "Mu", form: "Te", level: 15,
-        range: getRdt(allRdts.find((rdt) => (rdt.type === "Range" && rdt.name === "Touch"))) as RDT<"Range">[],
-        duration: getRdt(allRdts.find((rdt) => (rdt.type === "Duration" && rdt.name === "Concentration"))) as RDT<"Duration">[],
-        target: getRdt(allRdts.find((rdt) => (rdt.type === "Target" && rdt.name === "Part"))) as RDT<"Target">[],
+        range: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Range" && rdt.name === "Touch"))) as RDT<"Range">[],
+        duration: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Duration" && rdt.name === "Concentration"))) as RDT<"Duration">[],
+        target: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Target" && rdt.name === "Part"))) as RDT<"Target">[],
         description: "Change the stone touched by the caster into clay allowing manipulating it"
 
     },
     {
         guid: randomUUID(),
         name: "Lampi without Flame", technique: "Cr", form: "Ig", level: 15,
-        range: getRdt(allRdts.find((rdt) => (rdt.type === "Range" && rdt.name === "Touch"))) as RDT<"Range">[],
-        duration: getRdt(allRdts.find((rdt) => (rdt.type === "Duration" && rdt.name === "Concentration"))) as RDT<"Duration">[],
-        target: getRdt(allRdts.find((rdt) => (rdt.type === "Target" && rdt.name === "Individual"))) as RDT<"Target">[],
+        range: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Range" && rdt.name === "Touch"))) as RDT<"Range">[],
+        duration: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Duration" && rdt.name === "Concentration"))) as RDT<"Duration">[],
+        target: getSpellModelRdt(allRdts.find((rdt) => (rdt.type === "Target" && rdt.name === "Individual"))) as RDT<"Target">[],
         description: "Create a directionless light equal to lamplight."
 
     }
 
 ];
 
+/**
+ * Get all spells. 
+ * @returns All spells of the system.
+ */
 export async function getAllSpells(): Promise<SpellModel[]> {
 
     const spells = await createApiConnection().then(
 
         (dbh) => {
-            return dbh.query<{guid:UUID, value: ApiSpellModel}>("select guid, value from api_spells").then(
+            return dbh.query<{ guid: UUID, value: ApiSpellModel }>("select guid, value from api_spells").then(
                 (result) => {
                     console.log("Got %d spells from database", result.rowCount ?? 0);
-                    return Promise.all(result.rows.map( async (entry: {guid: UUID, value: ApiSpellModel}) => {
-                        return {...(await convertApiToSpell(entry.value)), guid: entry.guid};
+                    return Promise.all(result.rows.map(async (entry: { guid: UUID, value: ApiSpellModel }) => {
+                        return { ...(await convertApiToSpell(entry.value)), guid: entry.guid };
                     }));
                 }
-            ).finally( () => { safeRelease(dbh)});
+            ).finally(() => { safeRelease(dbh) });
         }
-    ).catch( (error) => {
+    ).catch((error) => {
         logger.error(error, "Could not retrieve spells");
         return [...spellStore];
     })
@@ -132,6 +137,7 @@ export type NewApiSpellModel = Omit<ApiSpellModel, "guid">
  */
 export async function convertSpellToApi(spell: SpellModel | NewSpellModel): Promise<ApiSpellModel | NewApiSpellModel> {
 
+    console.group("Converting spell");
     // Create RDTs for the RDTs not yet implemented.
     async function createRdtIfNecessary<TYPE extends string>(rdts: RDT<TYPE>[] | undefined): Promise<RDT<TYPE>[]> {
         return Promise.all([...(rdts ?? [])].map(async rdt => {
@@ -162,11 +168,15 @@ export async function convertSpellToApi(spell: SpellModel | NewSpellModel): Prom
     };
     if ("guid" in spell && validUUID(spell.guid)) {
         // Not a new spell.
+        console.info("Converted an existing spell %s with existing guid %s", spellSignature(spell), spell.guid);
+        console.groupEnd();
         return {
             ...result,
             guid: spell.guid as UUID
         }
     } else {
+        console.info("Converted a new spell", spellSignature(spell));
+        console.groupEnd();
         return result;
     }
 }
@@ -177,7 +187,7 @@ export async function convertSpellToApi(spell: SpellModel | NewSpellModel): Prom
  */
 export async function convertApiToSpell(spellApi: ApiSpellModel): Promise<SpellModel> {
 
-    console.group("Covnerting spell");
+    console.group("Converting spell");
 
     /**
      * Get art key, if needed.
@@ -250,17 +260,19 @@ export async function createSpell(spellModel: SpellModel | NewSpellModel) {
         const headers = new Headers();
         headers.append("Content-Type", "application/json");
         headers.append("Accept", "application/json");
-        if (process.env.API_KEY) {
-            headers.append("x-openapi-token", process.env.API_KEY);
+        if (process.env.DATA_API_KEY) {
+            // Using program api key.
+            headers.append("x-openapi-token", process.env.DATA_API_KEY);
         }
-        return await fetch("http://localhost:" + (process.env.PORT ?? "3000") + "//arm5/spells", {
+        return await fetch("/arm5/spells", {
             method: "POST",
-            body: JSON.stringify(apiSpell)
+            body: JSON.stringify(apiSpell),
+            headers
         }).then(
             (result) => {
                 if (result.ok) {
                     const uuid = result.json();
-                    logger.info("Created spell with %s UUID: %s", validUUID(uuid) ? "valid": "invalid", uuid);
+                    logger.info("Created spell %s with %s UUID: %s", spellSignature(spellModel), validUUID(uuid) ? "valid" : "invalid", uuid);
                     return checkUUID(uuid);
                 }
                 logger.error("Status: %d: %s", result.status, result.statusText);
@@ -276,27 +288,70 @@ export async function createSpell(spellModel: SpellModel | NewSpellModel) {
     }
 }
 
+/**
+ * Store spell to database. 
+ * @param spells The spell to store. 
+ * @returns Promise of UUIDs of the spells in database. 
+ */
 export async function storeDbSpells(spells: SpellModel[]) {
     return await createApiConnection().then(
         async dbh => {
-            dbh.query("begin");
+            const transactionInfo: TransactionOptions = {};
+            transactionInfo.id = await startTransaction(dbh, transactionInfo);
             return Promise.all(spells.map(async spell => {
-                const dbSpell = await convertSpellToApi(spell).then( (result) => (JSON.stringify(result)));
+                const dbSpell = await convertSpellToApi(spell).then((result) => (JSON.stringify(result)));
                 if (spell.guid) {
-                    dbh.query("INSERT INTO spells(guid, value) VALUES ($1, $2) ON CONFLICT DO UPDATE SET value = $2 WHERE guid = $1", [spell.guid, JSON.stringify(dbSpell)]);
+                    dbh.query("INSERT INTO api_spells(guid, value) VALUES ($1, $2) ON CONFLICT api_spells_pkey DO UPDATE SET value = $2 WHERE guid = $1", [spell.guid, dbSpell]);
                 } else {
-
+                    dbh.query("INSERT INTO api_spells(guid, value) VALUES (gen_random_uuid(), $1)", [spell]);
                 }
-            })).then( async (result) => {
-                await dbh.query("commit");
-                dbh.release();
+            })).then(async (result) => {
+                await commitTransaction(dbh, transactionInfo);
                 return result;
-            }, (error) => {
-                console.error("Inserting spells failed:%s", error);
-                dbh.query("rollback");
-                dbh.release();
-                throw new Error("Could not add spells", {cause: error});
+            }, async (error) => {
+                console.error("Storing spells failed:%s", error);
+                await rollbackTransaction(dbh, transactionInfo);
+                throw new Error("Could not add spells", { cause: error });
+            }).finally(() => {
+                safeRelease(dbh);
             })
+        });
+}
+
+/**
+ * Store signle spell to the database.
+ * @param spell The stored spell.
+ * @returns The promise of UUID of the stored spell. 
+ */
+export async function storeDbSpell(spell: SpellModel) {
+    return await convertSpellToApi(spell).then(
+        (apiSpell) => (createApiConnection().then(
+            async dbh => {
+                dbh.query("INSERT INTO api_spells(guid, value) VALUES ($1, $2) ON CONFLICT (guid) DO UPDATE SET value = EXCLUDED.value RETURNING guid",
+                    [spell.guid ?? randomUUID(), apiSpell]
+                ).then(
+                    (result) => {
+                        if (result.rowCount) {
+                            return result.rows[0].guid;
+                        } else {
+                            // Spell insertion or update failed. 
+                            console.error("Could not update existing spell %s with guid %s", spellSignature(spell), spell.guid);
+                            throw Error("Could not update existing spell");
+                        }
+                    }
+                );
+            },
+            (error: DatabaseError) => {
+                console.error("Storing spell %s to database failed: %s", spellSignature(spell), error);
+                /**
+                 * @todo Add error desginating the cause is database connection failure. 
+                 */
+                throw new Error("Database connection unavailable");
+            }
+        )),
+        (error) => {
+            console.error("Conversion of spell %s failed", spellSignature(spell));
+            throw error;
         });
 }
 
@@ -319,13 +374,22 @@ export async function storeSpells(spells: SpellModel[], altered?: UUID[]) {
                         reservedUUIDs.splice(index, 1);
                     }
                     console.log("Using UUID %s of the spell", spell.guid);
-                    spells.push(spell);
+                    await storeDbSpell(spell).catch( (error) => {
+                        console.error("Storing spell %s with guid %s to database failed: %s", spellSignature(spell), spell.guid, error);
+                        console.log("Storing spell to the memory cache");
+                        spells.push(spell);
+                        return spell.guid;
+                    });
                 } else {
-                    const uuid = await getUUID();
-                    console.log("Adding spell with new uuid %s", uuid);
-                    spells.push({ ...spell, guid: uuid });
-                    const rindex = reservedUUIDs.indexOf(uuid);
-                    reservedUUIDs.splice(rindex, 1);
+                    await storeDbSpell(spell).catch( 
+                        async (error) => {
+                        console.error("Storing new spell %s to database failed: %s", spellSignature(spell), error);
+                        console.log("Storing spell to the memory cache");
+                        const uuid = await getUUID();
+                        console.log("Adding spell with new uuid %s", uuid);
+                        spells.push({...spell, guid: uuid});
+                        return uuid;
+                    });
                 }
             }
         );
@@ -333,17 +397,36 @@ export async function storeSpells(spells: SpellModel[], altered?: UUID[]) {
         // Changing all spells. 
         const oldReserved = reservedUUIDs;
         reservedUUIDs = [...oldReserved];
-        spellStore = await Promise.all(spells.map(async (spell) => {
-            if (validUUID(spell.guid)) {
-                console.log("Using UUID %s of the spell", spell.guid);
-                return spell;
-            } else {
-                const uuid = await getUUID();
-                console.log("Adding spell with new uuid %s", uuid);
-                return { ...spell, guid: uuid };
+        const newSpells = [];
+        spells.forEach(
+            async (spell) => {
+                if (validUUID(spell.guid)) {
+                    const uuid = checkUUID(spell.guid);
+                    const index = reservedUUIDs.indexOf(uuid);
+                    if (index >= 0) {
+                        // Remove guid from rerverd guids. 
+                        reservedUUIDs.splice(index, 1);
+                    }
+                    console.log("Using UUID %s of the spell", spell.guid);
+                    await storeDbSpell(spell).catch( (error) => {
+                        console.error("Storing spell %s with guid %s to database failed: %s", spellSignature(spell), spell.guid, error);
+                        console.log("Storing spell to the memory cache");
+                        newSpells.push(spell);
+                        return spell.guid;
+                    });
+                } else {
+                    await storeDbSpell(spell).catch( 
+                        async (error) => {
+                        console.error("Storing new spell %s to database failed: %s", spellSignature(spell), error);
+                        console.log("Storing spell to the memory cache");
+                        const uuid = await getUUID();
+                        console.log("Adding spell with new uuid %s", uuid);
+                        newSpells.push({...spell, guid: uuid});
+                        return uuid;
+                    });
+                }
             }
-        }));
-        reservedUUIDs = oldReserved;
+        );
     }
 
 }
